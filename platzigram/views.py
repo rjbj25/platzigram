68e34966c8db26b797abdc8c4b69539fe036cd61
Ramiro Balmaceda Windows <ramirobj.25@gmail.com>
2021-08-12 16:26:00 -0500

    created server and app

---
+++ platzigram/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from datetime import date, datetime
+
+
+def hello_world(request):
+    now = datetime.now().strftime('%d/%m/%Y')
+    return HttpResponse(f'There is it at {str(now)}')
+
+
+def hi(request):
+    import pdb; pdb.set_trace()
+    return HttpResponse('Hi')
