3bd5c996e847764b34d26b12c434c903809e362c
Ramiro Balmaceda <ramirobj.25@gmail.com>
2021-08-15 19:23:58 -0500

    Edit Models

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    """Clase Model"""
+    email = models.EmailField()
+    password = models.CharField()
+
+    first_name = models.CharField()
+    last_name = models.CharField()
+
+    bio = models.TextField()
+
+    birthdate = models.DateField()
+
+    
