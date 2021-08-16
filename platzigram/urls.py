d84bd4c9c1a1126f2156d62e779a24e877319c2d
Ramiro Balmaceda <ramirobj.25@gmail.com>
2021-08-15 22:21:30 -0500

    Borrar test

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from django.contrib import admin
 from platzigram import views as local_views
 from posts import views as post_views
 
 urlpatterns = [
     path('hello-world/', local_views.hello_world),
     path('hi/', local_views.hi),
-    path('posts/', post_views.list_posts)
+    path('posts/', post_views.list_posts),
+    path('admin/', admin.site.urls)
 ]
